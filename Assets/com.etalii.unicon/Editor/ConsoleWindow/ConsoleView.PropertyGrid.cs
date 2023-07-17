392a251279dc398f9d519b94b5283bc36333cb49
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-17 11:25:22 +0200

    Added PropertyGrid.

---
+++ Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleView.PropertyGrid.cs
@@ -0,0 +1,43 @@
+namespace EtAlii.UniCon.Editor
+{
+    using Serilog.Events;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+
+    public partial class ConsoleView
+    {
+        private VisualElement BuildPropertyGrid(LogEvent logEvent)
+        {
+            var grid = new VisualElement
+            {
+                name = "Grid",
+                style =
+                {
+                    flexGrow = 1,
+                    flexDirection = FlexDirection.Column
+                }
+            };
+
+            //foreach (var property in logEvent.Properties.OrderBy(p => p.Key))
+            foreach (var property in logEvent.Properties)
+            {
+                var row = new VisualElement
+                {
+                    name = $"row-{property.Key}",
+                    style =
+                    {
+                        alignItems = Align.FlexStart,
+                        flexGrow = 1, 
+                        flexDirection = FlexDirection.Row
+                    }
+                    
+                };
+                row.contentContainer.Add(new Label { name = $"key-{property.Key}", text = property.Key, style = { flexGrow = 0, width = 200, unityTextAlign = TextAnchor.MiddleLeft }});
+                row.contentContainer.Add(new Label { name = $"value-{property.Key}", text = property.Value.ToString(), style = { flexGrow = 1, alignSelf = Align.FlexStart, unityTextAlign = TextAnchor.MiddleLeft }});
+                grid.contentContainer.Add(row);
+            }
+
+            return grid;
+        }
+    }    
+}
--- Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleView.cs
+++ Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleView.cs
@@ -5,7 +5,7 @@
     using UnityEngine;
     using UnityEngine.UIElements;
 
-    public class ConsoleView : VisualElement
+    public partial class ConsoleView : VisualElement
     {
         private readonly ListView _listView;
 
@@ -34,29 +34,29 @@
             _listView = this.Q<ListView>();
             _listView.itemsSource = _items;
             
-            _listView.makeItem = () => new Foldout { style =
-            {
-                flexGrow = 1,
-                unityFontDefinition = StyleKeyword.Initial,
-                unityFont = new StyleFont(_consoleFont)
-            }, value = false };
+            _listView.makeItem = () => new Foldout 
+            { 
+                style =
+                {
+                    flexGrow = 1,
+                    unityFontDefinition = StyleKeyword.Initial,
+                    unityFont = new StyleFont(_consoleFont)
+                }, 
+                value = false 
+            };
             _listView.bindItem = (e, i) => Bind((Foldout)e, _items[i]);
-            _listView.unbindItem = (e, _) => Unbind((Foldout)e);
-        }
-
-        private void Unbind(Foldout foldout)
-        {
-            foldout.contentContainer.Clear();
         }
 
         private void Bind(Foldout foldout, LogEventLineViewModel viewModel)
         {
-            foldout.text = LogEventLine.GetMessage(viewModel);
-            foldout.contentContainer.Add(new Label
+            if (foldout.userData as LogEventLineViewModel != viewModel)
             {
-                style = { flexGrow = 1, whiteSpace = WhiteSpace.Normal }, 
-                text = "Lorem ipsum dolor sit amet. In rerum sapiente ea voluptas officia qui internos rerum est optio unde et Quis autem et deleniti architecto in perferendis reprehenderit! Ut sunt inventore sit nemo unde est magni sequi ut incidunt maiores. Et excepturi dolor cum obcaecati autem vel beatae error ea voluptas quia aut praesentium autem qui officiis ullam est obcaecati voluptatem."
-            });
+                foldout.value = false;
+                foldout.text = LogEventLine.GetMessage(viewModel);
+                foldout.contentContainer.Clear();
+                foldout.contentContainer.Add(BuildPropertyGrid(viewModel.LogEvent));
+                foldout.userData = viewModel;
+            }
         }
         
         public void Bind(ConsoleViewModel viewModel)
