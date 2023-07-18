8ecbff416a2431ff6facd1c9432f5e6107ca2c92
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-18 17:15:29 +0200

    Made UniConStartup configurable (people most probably want to extend/change their Serilog configuration.

---
+++ Assets/com.etalii.unicon/Startup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Serilog;
+using Serilog.Sinks.UniCon;
+
+public static partial class UniConStartup
+{
+    // Debug initialization should happen as early as possible, before any other system is started.
+#if DEBUG
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+#else 
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen]     
+#endif
+    private static void Load()
+    {
+        if (Log.Logger == Serilog.Core.Logger.None)
+        {
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Verbose();
+
+            // ReSharper disable once InvocationIsSkipped
+            // This method is used as it provides a way to fine-tune the UniCon Serilog configuration. 
+            ConfigureLogging(loggerConfiguration);
+            
+            Log.Logger = loggerConfiguration
+                .WriteTo.UniCon()
+                .CreateLogger();
+        }
+    }
+
+    // ReSharper disable once PartialMethodWithSinglePart
+    /// <summary>
+    /// Implement this method to customize the UniCon Serilog configuration. For example to also log to other systems
+    /// e.g. Seq, or to file.  
+    /// </summary>
+    /// <param name="loggerConfiguration"></param>
+    static partial void ConfigureLogging(LoggerConfiguration loggerConfiguration);
+}
