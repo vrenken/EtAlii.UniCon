b1a4e8991985b7f250026516b68f4e89a1d0b30b
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-19 00:46:35 +0200

    Added fake test data using Bogus.

--- Assets/Tests/Test 1/SerilogLogTicker.cs
+++ Assets/Tests/Test 1/SerilogLogTicker.cs
@@ -4,10 +4,13 @@
     using UnityEngine;
     using Serilog;
     using System.Collections;
+    using Bogus.DataSets;
     using Random = UnityEngine.Random;
 
     public class SerilogLogTicker : MonoBehaviour
     {
+        private readonly Lorem _lorem = new();
+        
         private Serilog.ILogger _logger;
 
         public float interval = 1;
@@ -28,14 +31,26 @@
             while (gameObject.activeSelf)
             {
                 yield return new WaitForSeconds(interval);
-                switch (Random.Range(0,6))
+                switch (Random.Range(0,16))
                 {
+                    // ReSharper disable TemplateIsNotCompileTimeConstantProblem
                     case 0: _logger.Verbose("New verbose log entry {Property1}, and {Property2}", Environment.TickCount, "something"); break;
                     case 1: _logger.Information("New information log entry"); break;
                     case 2: _logger.Debug("New debug log entry"); break;
                     case 3: _logger.Error("New error log entry"); break;
                     case 4: _logger.Warning("New warning log entry"); break;
                     case 5: _logger.Fatal("New fatal log entry {FatalError}", "Some weird crash"); break;
+                    case 6: _logger.Information("Another log entry with some new {Information} from a specific {Time}", "Well known information", DateTimeOffset.Now); break;
+                    case 7: _logger.Debug("Starting some more debugging"); break;
+                    case 8: _logger.Verbose(_lorem.Sentence()); break;
+                    case 9: _logger.Verbose(_lorem.Sentence()); break;
+                    case 10: _logger.Verbose(_lorem.Sentence()); break;
+                    case 11: _logger.Debug(_lorem.Sentence()); break;
+                    case 12: _logger.Information(_lorem.Sentence()); break;
+                    case 13: _logger.Warning(_lorem.Sentence()); break;
+                    case 14: _logger.Information(_lorem.Sentence()); break;
+                    case 15: _logger.Debug(_lorem.Sentence()); break;
+                    // ReSharper restore TemplateIsNotCompileTimeConstantProblem
                 }
             }
         }
--- Assets/Tests/Test 1/UnityLogTicker.cs
+++ Assets/Tests/Test 1/UnityLogTicker.cs
@@ -3,10 +3,13 @@
     using System;
     using UnityEngine;
     using System.Collections;
+    using Bogus.DataSets;
     using Random = UnityEngine.Random;
 
     public class UnityLogTicker : MonoBehaviour
     {
+        private readonly Lorem _lorem = new();
+
         public float interval = 1;
         private void OnEnable()
         {
@@ -24,13 +27,23 @@
             while (gameObject.activeSelf)
             {
                 yield return new WaitForSeconds(interval);
-                switch (Random.Range(0,5))
+                switch (Random.Range(0,13))
                 {
                     case 0: Debug.Log("New information log entry", this); break;
                     case 1: Debug.LogException(new InvalidOperationException("Some invalid operation"), this); break;
                     case 2: Debug.LogError("New error log entry", this); break;
                     case 3: Debug.LogWarning("New warning log entry", this); break;
                     case 4: Debug.LogAssertion("New assertion log entry", this); break;
+                    case 5: Debug.Log(_lorem.Sentence()); break;
+                    case 6: Debug.Log(_lorem.Sentence()); break;
+                    case 7: Debug.Log(_lorem.Sentence()); break;
+                    case 8: Debug.Log(_lorem.Sentence()); break;
+                    case 9: Debug.LogWarning(_lorem.Sentence()); break;
+                    case 10: Debug.Log(_lorem.Sentence()); break;
+                    case 11: Debug.LogWarning(_lorem.Sentence()); break;
+                    case 12: Debug.Log(_lorem.Sentence()); break;
+                    // ReSharper restore TemplateIsNotCompileTimeConstantProblem
+
                 }
             }
         }
