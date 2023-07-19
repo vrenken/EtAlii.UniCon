c41b14f3b9a6cb78d54c5fe7c3ff7d5cb624449a
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-19 23:26:23 +0200

    Added comments and tooltips.

--- Assets/com.etalii.unicon/Editor/ConsoleWindow/_Model/ConsoleSettings.cs
+++ Assets/com.etalii.unicon/Editor/ConsoleWindow/_Model/ConsoleSettings.cs
@@ -4,9 +4,24 @@ namespace EtAlii.UniCon.Editor
     {
         public LogLevel LogLevel = LogLevel.Information | LogLevel.Debug | LogLevel.Warning | LogLevel.Error | LogLevel.Fatal;
 
+        /// <summary>
+        /// Set this value to true to show any log event that contains information about an exception. This overrules the default log levels.
+        /// </summary>
+        public bool ShowExceptions = true;
+        
+        /// <summary>
+        /// Set this value to true to show all log events that are gathered by Serilog. 
+        /// </summary>
         public bool UseSerilogSource = true;
+
+        /// <summary>
+        /// Set this value to true to show all log events that are gathered by the default Unity debug logs. 
+        /// </summary>
         public bool UseUnitySource = false;
 
+        /// <summary>
+        /// Set this property to true to keep scrolling to any new log events received.
+        /// </summary>
         public bool ScrollToTail = true;
 
         public bool ShowFilter = true;
