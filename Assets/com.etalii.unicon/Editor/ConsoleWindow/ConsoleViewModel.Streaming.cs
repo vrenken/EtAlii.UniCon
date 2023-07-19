0edcf4742c2a6b3505d40d90e157e39b9a3e7404
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-19 18:00:28 +0200

    Added very simple rule filter mechanism.

--- Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleViewModel.Streaming.cs
+++ Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleViewModel.Streaming.cs
@@ -41,18 +41,35 @@ namespace EtAlii.UniCon.Editor
                     return false;
                 })
                 .Where(logEvent =>
-            {
-                return logEvent.Level switch
                 {
-                    LogEventLevel.Verbose => Settings.LogLevel.HasFlag(LogLevel.Verbose),
-                    LogEventLevel.Information => Settings.LogLevel.HasFlag(LogLevel.Information),
-                    LogEventLevel.Debug => Settings.LogLevel.HasFlag(LogLevel.Debug),
-                    LogEventLevel.Warning => Settings.LogLevel.HasFlag(LogLevel.Warning),
-                    LogEventLevel.Error => Settings.LogLevel.HasFlag(LogLevel.Error),
-                    LogEventLevel.Fatal => Settings.LogLevel.HasFlag(LogLevel.Fatal),
-                    _ => throw new ArgumentOutOfRangeException(nameof(logEvent.Level))
-                };
-            });
+                    return logEvent.Level switch
+                    {
+                        LogEventLevel.Verbose => Settings.LogLevel.HasFlag(LogLevel.Verbose),
+                        LogEventLevel.Information => Settings.LogLevel.HasFlag(LogLevel.Information),
+                        LogEventLevel.Debug => Settings.LogLevel.HasFlag(LogLevel.Debug),
+                        LogEventLevel.Warning => Settings.LogLevel.HasFlag(LogLevel.Warning),
+                        LogEventLevel.Error => Settings.LogLevel.HasFlag(LogLevel.Error),
+                        LogEventLevel.Fatal => Settings.LogLevel.HasFlag(LogLevel.Fatal),
+                        _ => throw new ArgumentOutOfRangeException(nameof(logEvent.Level))
+                    };
+                }).Where(logEvent =>
+                {
+                    foreach (var rule in FilterRules)
+                    {
+                        if (logEvent.Properties.TryGetValue(rule.Property, out var propertyValue))
+                        {
+                            if (propertyValue.ToString() != rule.Value.ToString())
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                });
             
             Stream = stream;
             StreamChanged?.Invoke();            
