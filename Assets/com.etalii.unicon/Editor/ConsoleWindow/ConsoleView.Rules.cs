d97b3ab25051841b0ed11531ca4db744fb1078ea
Peter Vrenken <peter.vrenken@accenture.com>
2023-07-19 18:00:11 +0200

    Made sure that rules get visualized (nothing is done with them (yet)

--- Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleView.Rules.cs
+++ Assets/com.etalii.unicon/Editor/ConsoleWindow/ConsoleView.Rules.cs
@@ -1,13 +1,17 @@
 namespace EtAlii.UniCon.Editor
 {
+    using System;
+    using System.Linq;
     using RedMoon.ReactiveKit;
     using UniRx;
+    using UnityEngine;
     using UnityEngine.UIElements;
 
     public partial class ConsoleView
     {
         private readonly Button _rulesButton;
         private readonly VisualElement _rulesPanel;
+        private readonly ScrollView _rulesList;
 
         private void BindRules(ConsoleViewModel viewModel, CompositeDisposable disposable)
         {
@@ -31,6 +35,74 @@ namespace EtAlii.UniCon.Editor
                     UpdateToggleButton(_rulesButton, _viewModel.Settings.ShowRules);
                     UpdateRulesPanel();
                     break;
+                case nameof(_viewModel.FilterRules):
+                    UpdatesRulesList();
+                    break;
+            }
+        }
+
+        private void UpdatesRulesList()
+        {
+            _rulesList.Clear();
+            foreach (var filterRule in _viewModel.FilterRules.OrderBy(r => r.Property))
+            {
+                var filterRuleView = new VisualElement
+                {
+                    style = 
+                    {
+                        flexGrow = 1, 
+                        flexDirection = FlexDirection.Row,
+                        width = new StyleLength(new Length(100, LengthUnit.Percent)),
+                        height = new StyleLength(StyleKeyword.Auto)
+                    }
+                };
+
+                var propertyLabel = new Label
+                {
+                    text = filterRule.Property,
+                    style =
+                    {
+                        unityTextAlign = TextAnchor.MiddleLeft
+                    }
+                };
+                filterRuleView.contentContainer.Add(propertyLabel);
+
+                var dropDown = new DropdownField
+                {
+                    value = filterRule.FilterType.ToString(),
+                    choices = Enum.GetNames(typeof(FilterType)).ToList(),
+                };
+                filterRuleView.contentContainer.Add(dropDown);
+
+                var valueTextField = new TextField
+                {
+                    multiline = false,
+                    value = filterRule.Value.ToString().Trim('"'),
+                    style =
+                    {
+                        flexGrow = 1
+                    }
+                };
+                filterRuleView.contentContainer.Add(valueTextField);
+                
+                var removeButton = new Button
+                {
+                    text = $"<b><color=red>\u00D7</color></b>",
+                    focusable = false,
+                    style =
+                    {
+                        marginLeft = 0, marginRight = 0,
+                        width = 8, maxWidth = 8,
+                        backgroundColor = Color.clear,
+                        borderBottomWidth = 0,
+                        borderLeftWidth = 0,
+                        borderTopWidth = 0,
+                        borderRightWidth = 0
+                    }
+                };
+                filterRuleView.contentContainer.Add(removeButton);
+
+                _rulesList.contentContainer.Add(filterRuleView);
             }
         }
 
