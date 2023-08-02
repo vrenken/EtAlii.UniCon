628fef8701087cdebc4b489cde453940033aa5a5
Peter Vrenken <peter.vrenken@accenture.com>
2023-08-02 18:05:00 +0200

    Added some new WellKnownProperties.

--- Assets/com.etalii.unicon/Runtime/Aggregration/_Model/WellKnownProperties.cs
+++ Assets/com.etalii.unicon/Runtime/Aggregration/_Model/WellKnownProperties.cs
@@ -4,5 +4,7 @@ namespace EtAlii.UniCon
     {
         public const string Prefix = "UNICON_";
         public const string IsUnityLogEvent = Prefix + "IS_UNITY_LOG_EVENT";
+        public const string IsSerilogLogEvent = Prefix + "IS_SERILOG_LOG_EVENT";
+        public const string IsMicrosoftLogEvent = Prefix + "IS_MICROSOFT_LOG_EVENT";
     }
 }
