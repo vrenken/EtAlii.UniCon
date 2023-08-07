78493a325397ff892c7b18c33166265389a78293
Peter Vrenken <peter.vrenken@accenture.com>
2023-08-07 10:23:38 +0200

    Shelve for temp version bump

---
+++ Assets/com.etalii.unicon/Editor/Console/Streaming/_Model/StreamConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace EtAlii.UniCon.Editor
+{
+    using System;
+    using EtAlii.Unicon;
+    using UniRx;
+
+    public class StreamConfiguration
+    {
+        public ReplaySubject<LogEntry> ForwardSubject;
+        public IObservable<LogEntry> Forward;
+        public CompositeDisposable ForwardPipeline;
+
+        public ReplaySubject<LogEntry> BackwardSubject;
+        public IObservable<LogEntry> Backward;
+        public CompositeDisposable BackwardPipeline;
+    }
+}
